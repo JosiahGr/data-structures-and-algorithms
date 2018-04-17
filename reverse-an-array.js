ad193cbe5de15014a7c77af88d018b4be4934078
Josiah Green <Josiahgreen90@gmail.com>
2018-04-17 16:06:43 -0700

    Delete reverse-an-array.js

--- reverse-an-array.js
+++
@@ -1,9 +0,0 @@
-function reverseArr(num) {
-  var newArray = [];
-  for(let i = num.length - 1; i >= 0; i--) {
-    newArray.push(num[i]);
-  }
-  return newArray;
-}
-
-reverseArr([1, 2, 3, 4, 5, 6]);
