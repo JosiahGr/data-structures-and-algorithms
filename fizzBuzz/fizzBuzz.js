a00be0704b2640513ca7d9422f90a85763429363
Josiah Green <JosiahGreen90@gmail.com>
2018-05-07 21:14:13 -0700

    tests pass

---
+++ fizzBuzz/fizzBuzz.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function fizzBuzz(rootNode) {
+  if (!rootNode) return undefined;
+
+  if (rootNode.value % 3 === 0 && rootNode.value % 5 === 0) {
+    rootNode.value = 'fizzBuzz';
+  } else if (rootNode.value % 5 === 0) {
+    rootNode.value = 'buzz';
+  } else if (rootNode.value % 3 === 0) {
+    rootNode.value = 'fizz';
+  }
+  fizzBuzz(rootNode.left);
+  fizzBuzz(rootNode.right);
+  return undefined;
+}
+
+export default fizzBuzz;
