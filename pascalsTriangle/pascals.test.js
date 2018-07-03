9d095233be7cf99fc62b02bf2630267aa573df7b
Josiah Green <JosiahGreen90@gmail.com>
2018-07-03 11:11:55 -0700

    all tests pass

--- pascalsTriangle/pascals.test.js
+++ pascalsTriangle/pascals.test.js
@@ -1,7 +1,13 @@
 import pascalsTriangle from './pascals';
 
-describe('#pascalsTriangle should return each matched values as a new queue', () => {
-  test('should return with 1, 1, 1, 1, 1, 1', () => {
+describe('#pascalsTriangle', () => {
+  test('should return 3 rows of Pascals Triangle', () => {
     expect(pascalsTriangle(3)).toEqual([[1], [1, 1], [1, 2, 1]]);
+  });
+  test('should return 5 rows of Pascals Triangle', () => {
+    expect(pascalsTriangle(5)).toEqual([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]);
+  });
+  test('should return the first two rows of Pascals Triangle', () => {
+    expect(pascalsTriangle(2)).toEqual([[1], [1, 1]]);
   });
 });
