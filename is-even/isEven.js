f99d9a109a7c2666ab5d08924236368ec826f196
Josiah Green <JosiahGreen90@gmail.com>
2018-09-15 15:53:42 -0700

    tests passing

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -2,13 +2,9 @@
 
 export default function isEven(n) {
   let flag = true;
-  const num = n.toString().split(' ');
+  const num = n.toString().split('');
 
   for (let i = 0; i < num.length; i++) {
-    // if (isNaN(num[i])) {
-    //   flag = false;
-    //   break;
-    // }
     if (num[i] % 2 !== 0) {
       flag = false;
       break;
