8f10dc7acb809ea631ab39b6dd4c8c87d2087aa2
Josiah Green <JosiahGreen90@gmail.com>
2018-09-18 13:09:33 -0700

    test written

---
+++ multiply-strings/multiplyStrings.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import multiplyStr from './multiplyStrings';
+
+describe('#multiplyStr', () => {
+  test('Should return product of two numbers', () => {
+    expect(multiplyStr('2', '2', '0')).toEqual('4');
+    expect(multiplyStr('3', '0', '4')).toEqual('12');
+    expect(multiplyStr('0', '1', '1')).toEqual('1');
+  });
+  test('Should return product of three numbers', () => {
+    expect(multiplyStr('2', '2', '2')).toEqual('8');
+    expect(multiplyStr('3', '3', '4')).toEqual('36');
+    expect(multiplyStr('10', '10', '2')).toEqual('200');
+  });
+  test('Should return undefined for non integer', () => {
+    expect(multiplyStr('a', '2', '2')).toEqual(undefined);
+  });
+});
