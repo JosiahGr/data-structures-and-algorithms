9d9c147f8de4bf45a8e012b564cc09abb446af5b
Josiah Green <JosiahGreen90@gmail.com>
2018-09-18 13:21:49 -0700

    tests pass

--- multiply-strings/multiplyStrings.test.js
+++ multiply-strings/multiplyStrings.test.js
@@ -3,17 +3,12 @@
 import multiplyStr from './multiplyStrings';
 
 describe('#multiplyStr', () => {
-  test('Should return product of two numbers', () => {
-    expect(multiplyStr('2', '2')).toEqual('4');
-    expect(multiplyStr('3', '4')).toEqual('12');
-    expect(multiplyStr('1', '1')).toEqual('1');
-  });
   test('Should return product of three numbers', () => {
     expect(multiplyStr('2', '2', '2')).toEqual('8');
     expect(multiplyStr('3', '3', '4')).toEqual('36');
     expect(multiplyStr('10', '10', '2')).toEqual('200');
   });
-  test('Should return undefined for non integer', () => {
-    expect(multiplyStr('a', '2', '2')).toEqual(undefined);
+  test('Should return NaN for non integer', () => {
+    expect(multiplyStr('a', '2', '2')).toEqual('NaN');
   });
 });
