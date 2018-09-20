f4efa0eca36da7a6e5d29458163e968c56a931cd
Josiah Green <JosiahGreen90@gmail.com>
2018-09-20 13:41:38 -0700

    solution passes

--- highest-product/highestProduct.test.js
+++ highest-product/highestProduct.test.js
@@ -5,6 +5,7 @@ import product from './highestProduct';
 describe('#product', () => {
   test('Should return max product value of three integers', () => {
     expect(product([1, 2, 3, 4, 5])).toEqual(60);
-    expect(product([0, -2, 0, 4, 5])).toEqual(20);
+    expect(product([0, -2, 0, 4, 5])).toEqual(0);
+    expect(product([6, -2, 0, 4, 5])).toEqual(120);
   });
 });
