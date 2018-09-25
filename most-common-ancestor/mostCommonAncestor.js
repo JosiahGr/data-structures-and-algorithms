ed0d8e811da6121d536a10bfa4c24d5ee50ed101
Josiah Green <JosiahGreen90@gmail.com>
2018-09-24 23:39:44 -0700

    solution added

---
+++ most-common-ancestor/mostCommonAncestor.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const commonAncestor = (array) => {
+  const Map = {};
+  let maxEl = array[0]; 
+  let maxCount = 1;
+
+  for (let i = 0; i < array.length - 1; i++) {
+    const el = array[i];
+
+    if (Map[el] === null) {
+      Map[el] = 1;
+    } else {
+      Map[el] += 1;
+    }
+    if (Map[el] > maxCount) {
+      maxEl = el;
+      maxCount += 1;
+    }
+  }
+};
+
+export default commonAncestor;
