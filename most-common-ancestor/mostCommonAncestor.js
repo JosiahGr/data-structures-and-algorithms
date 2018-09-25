0c0a75c8348f83002606738e39b496e9d0e8287a
Josiah Green <JosiahGreen90@gmail.com>
2018-09-25 10:09:47 -0700

    tests pass

--- most-common-ancestor/mostCommonAncestor.js
+++ most-common-ancestor/mostCommonAncestor.js
@@ -1,23 +1,26 @@
 'use strict';
 
-const commonAncestor = (array) => {
+const commonAncestor = (arr) => {
   const Map = {};
+  const array = arr;
   let maxEl = array[0]; 
   let maxCount = 1;
 
   for (let i = 0; i < array.length; i++) {
     const el = array[i];
 
-    if (Map[el] === null) {
+    if (!Map[el]) {
       Map[el] = 1;
     } else {
       Map[el] += 1;
     }
     if (Map[el] > maxCount) {
-      maxEl = el;
+      maxEl = Map[el];
       maxCount += 1;
     }
   }
+
+  return maxEl;
 };
 
 export default commonAncestor;
--- most-common-ancestor/mostCommonAncestor.test.js
+++ most-common-ancestor/mostCommonAncestor.test.js
@@ -5,5 +5,7 @@ import commonAncestor from './mostCommonAncestor';
 describe('#commonAncestor', () => {
   test('Should return most common element in an array', () => {
     expect(commonAncestor([1, 2, 3, 4, 4, 3, 3])).toEqual(3);
+    expect(commonAncestor([1, 2, 4, 4, 3])).toEqual(4);
+    expect(commonAncestor(['apple', 'orange', 'apple', 'peach', 1])).toEqual('apple');
   });
 });
