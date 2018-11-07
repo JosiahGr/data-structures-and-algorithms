6983d7be2f18af4fc980fef10f65ad11d0e111de
Josiah Green <JosiahGreen90@gmail.com>
2018-11-07 11:29:38 -0800

    test passes

--- merge/mergeArray.js
+++ merge/mergeArray.js
@@ -1,13 +1,14 @@
 'use strict';
 
 export default function mergeArray(arr1, arr2) {
-  let { length } = null;
+  let length;
+
   const newArr = [];
   
   if (arr1.length >= arr2.length) {
-    length = arr1;
+    length = arr1.length; //eslint-disable-line
   } else {
-    length = arr2;
+    length = arr2.length; // eslint-disable-line
   }
 
   for (let i = 0; i < length; i++) {
