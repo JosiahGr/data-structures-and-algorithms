644228c6a0e1cf0214c85d6b95336de1243a4828
Josiah Green <JosiahGreen90@gmail.com>
2018-11-10 19:10:12 -0800

    tests passing

--- telephone-number-verifier/phoneNumber.js
+++ telephone-number-verifier/phoneNumber.js
@@ -1,12 +1,12 @@
 'use strict';
 
 export default function verify(string) {
-  const phoneNumber = string.split('');
-
-  if (phoneNumber.length !== 10) {
+  if (string.length !== 10 || isNaN(string)) { // eslint-disable-line
     return false;
   }
-  
+
+  const phoneNumber = string.split('');
+
   const format = `(${phoneNumber[0]}${phoneNumber[1]}${phoneNumber[2]}) ${phoneNumber[3]}${phoneNumber[4]}${phoneNumber[5]}-${phoneNumber[6]}${phoneNumber[7]}${phoneNumber[8]}${phoneNumber[9]}`;
 
   return format;
