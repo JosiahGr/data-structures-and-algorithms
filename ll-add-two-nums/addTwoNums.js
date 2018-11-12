d71e8457bee7f2192e48845b7f43027e37ed585c
Josiah Green <JosiahGreen90@gmail.com>
2018-11-12 11:57:35 -0800

    fixed syntax error

--- ll-add-two-nums/addTwoNums.js
+++ ll-add-two-nums/addTwoNums.js
@@ -1,5 +1,22 @@
 'use strict';
 
+import Node from './node';
+
+function _add(value) { // eslint-disable-line
+  const length = value.length; // eslint-disable-line
+  let counter = 0;
+  value.split('');
+  const node = new Node(value[counter]);
+
+  let currentNode = node;
+  while (counter < length && currentNode.next) {
+    currentNode = currentNode.next;
+    counter += 1;
+    currentNode.next = node;
+  }
+  return this;
+}
+
 export default function add(ll1, ll2) {
   let nodeA = ll1.head;
   let nodeB = ll2.head;
@@ -21,7 +38,8 @@ export default function add(ll1, ll2) {
   num1 = parseInt(num1, 10);
   num2 = parseInt(num2, 10);
 
-  const result = num1 + num2;
+  let result = num1 + num2;
+  result = `${result}`;
 
-  return result;
+  return _add(result);
 }
