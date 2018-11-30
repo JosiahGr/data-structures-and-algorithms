4cf835b3ba0708ad95e33251ab8bc9c91a37b61a
Josiah Green <JosiahGreen90@gmail.com>
2018-11-30 12:43:16 -0800

    decimals now fixed to 2 and adjusting for small numbers

--- meal-total-calculator/mealTotal.js
+++ meal-total-calculator/mealTotal.js
@@ -1,13 +1,19 @@
 'use strict';
 
-export default function mealTotal(amount, tax, tip) {
+export default function mealTotal(amount, tip, tax) {
   let tipp = tip.replace('%', '');
-  tipp = `0.${tipp}`;
   let taxx = tax.replace('%', '');
-  taxx = `0.${taxx}`;
+  if (tipp < 10 || taxx < 10) {
+    tipp = `0.0${tipp}`;
+    taxx = `0.0${taxx}`;
+  } else {
+    taxx = `0.${taxx}`;
+    tipp = `0.${tipp}`;
+  }
 
-  const totalTax = amount * taxx;
-  const totalTip = amount * tipp;
+  const totalTax = taxx * amount;
+  const totalTip = tipp * amount;
+  const total = amount + totalTax + totalTip;
 
-  return Math.floor(amount + totalTax + totalTip);
+  return total.toFixed(2);
 }
