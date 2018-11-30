c59583a4e75300e2e250d82f31eabc4f715cb2eb
Josiah Green <JosiahGreen90@gmail.com>
2018-11-30 11:59:52 -0800

    tests added

---
+++ meal-total-calculator/mealTotal.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import mealTotal from './mealTotal';
+
+describe('#mealTotal', () => {
+  test('should return the total amount to pay when given the price and desired tip percentage.', () => {
+    expect(mealTotal(10, 10, 10)).toEqual(12);
+  });
+});
