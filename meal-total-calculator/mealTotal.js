5430ce7b60a37cff5bfb2bc21076dc195008e620
Josiah Green <JosiahGreen90@gmail.com>
2018-11-30 12:26:26 -0800

    solution now accepts strings as parameters for tip and tax percentages

--- meal-total-calculator/mealTotal.js
+++ meal-total-calculator/mealTotal.js
@@ -1,8 +1,13 @@
 'use strict';
 
 export default function mealTotal(amount, tax, tip) {
-  const totalTax = amount * tax;
-  const totalTip = amount * tip;
+  let tipp = tip.replace('%', '');
+  tipp = `0.${tipp}`;
+  let taxx = tax.replace('%', '');
+  taxx = `0.${taxx}`;
+
+  const totalTax = amount * taxx;
+  const totalTip = amount * tipp;
 
   return Math.floor(amount + totalTax + totalTip);
 }
