101ac1dbb70358b846aa0efb8649a5b71130aef4
Josiah Green <JosiahGreen90@gmail.com>
2018-12-12 23:34:55 -0800

    fixed test

--- add-two-sums-in-array/twoSums.test.js
+++ add-two-sums-in-array/twoSums.test.js
@@ -6,10 +6,10 @@ describe('#addSums', () => {
   test('Should return [0, 2]', () => {
     expect(addSums([1, 2, 3], 4)).toEqual([0, 2]);
   });
-  test('Should return [1, 3]', () => {
+  test('Should return [1, 3] because four is the combined sum', () => {
     expect(addSums([1, 2, 3, 4], 6)).toEqual([1, 3]);
   });
-  test('Should return [3, 4]', () => {
+  test('Should return [3, 4] because four is the combined sum of 4 and 5', () => {
     expect(addSums([1, 2, 3, 4, 5], 9)).toEqual([3, 4]);
   });
 });
